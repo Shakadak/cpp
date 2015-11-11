f7e6c88538deec837934fd3c057db2debdcfd210
nathanael <nathanael.pineau@gmail.com>
2015-11-11 15:22:51 +0100

    ex01

---
+++ pool/d07/ex01/iter.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <clocale>
+
+template<typename A, typename B, typename C>
+void    iter(A& xs, B const& l, C& f) {
+    for (B i = 0; i < l; i++) {
+        xs[i] = f(xs[i]);
+    }
+}
+
+static char toLower(char c) {
+    return (std::tolower(c));
+}
+
+static char toUpper(char c) {
+    return (std::toupper(c));
+}
+
+static int plusOne(int i) {
+    return (i + 1);
+}
+
+int main(void) {
+    {
+        std::string xs = "Hello World";
+        std::cout << "xs: " << xs << std::endl;
+        iter(xs, xs.length() / 2, toUpper);
+        std::cout << "iter(xs, xs.length() / 2, toUpper): " << xs << std::endl;
+    }
+    {
+        std::string xs = "Hello World";
+        std::cout << "xs: " << xs << std::endl;
+        iter(xs, xs.length(), toLower);
+        std::cout << "iter(xs, xs.length(), toLower): " << xs << std::endl;
+    }
+    {
+        int xs[4] = {1, 2, 3, 4};
+        std::cout << "xs: " << xs[0] << ", " << xs[1] << ", " << xs[2] << ", " << xs[3] << std::endl;
+        iter(xs, 4, plusOne);
+        std::cout << "iter(xs, 4, plusOne): " << xs[0] << ", " << xs[1] << ", " << xs[2] << ", " << xs[3] << std::endl;
+    }
+    return (0);
+}
